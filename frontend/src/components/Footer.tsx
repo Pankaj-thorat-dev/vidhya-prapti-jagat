fa97533e6c3f6d3c466aced61f297617d402caea
Pankaj-thorat-dev <pankajthorat99934@gmail.com>
2026-01-23 15:56:23 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -20,6 +20,16 @@ const Footer: React.FC = () => {
           </ul>
         </div>
 
+
+        <div className="footer-section">
+          <h4>Legal</h4>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/shop">Shop</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </div>
+
         <div className="footer-section">
           <h4>Support</h4>
           <ul>
